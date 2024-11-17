a59dd5e4423570bb6bbc5ae77009e37a120a5956
Vibha Hodachalli <95110043+vibha-h@users.noreply.github.com>
2024-11-17 11:49:49 -0500

    Started

--- lib/views/join_lobby_view.dart
+++ lib/views/join_lobby_view.dart
@@ -1,3 +1,31 @@
-class JoinLobbyView {
-  
+// lobby_view.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './home_view.dart';
+
+class LobbyView extends StatelessWidget {
+  const LobbyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lobby'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeView()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Welcome to the Lobby!'),
+      ),
+    );
+  }
 }
