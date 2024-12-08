64caefea1c271b80a250bc9d1440a08d5295165c
Mirla03 <112401952+Mirla03@users.noreply.github.com>
2024-12-08 18:10:52 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,43 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           fontFamily: 'Parkinsans',
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 143, 53, 29),
+            brightness: Brightness.light, // Light mode
+          ),
+          textTheme: const TextTheme(
+            headlineLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            bodyLarge: TextStyle(fontSize: 16),
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 233, 154, 142), // AppBar background color
+            foregroundColor: Colors.white, // AppBar text/icon color
+            elevation: 0,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 117, 34, 21), // Button background
+              foregroundColor: Colors.white, // Button text color
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor:Color.fromARGB(255, 117, 34, 21), // TextButton color
+            ),
+          ),
         ),
+        darkTheme: ThemeData(
+          fontFamily: 'Parkinsans',
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromARGB(255, 205, 105, 90),
+            brightness: Brightness.dark,
+          ),
+        ),
+        themeMode: ThemeMode.system, // Automatically switch between light/dark based on dark mode
         home: const LoginView(),
       ),
     );
